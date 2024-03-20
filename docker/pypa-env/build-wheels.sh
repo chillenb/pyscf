4b29099c3854805612a8506281d2004c89e399a7
chillenb <chillenbrand15@gmail.com>
2024-03-20 00:42:13 -0400

    pass build args to scikit-build-core in wheel build script

--- docker/pypa-env/build-wheels.sh
+++ docker/pypa-env/build-wheels.sh
@@ -7,10 +7,10 @@ dst=${GITHUB_WORKSPACE:-/src/pyscf}/linux-wheels
 mkdir -p /root/wheelhouse $src/linux-wheels
 
 if [ "$#" -gt 0 ]; then
-  export CMAKE_CONFIGURE_ARGS="-DWITH_F12=OFF -DBUILD_LIBXC=OFF -DBUILD_XCFUN=OFF -DBUILD_LIBCINT=OFF"
+  MY_BUILD_ARGS="-DWITH_F12=OFF;-DBUILD_LIBXC=OFF;-DBUILD_XCFUN=OFF;-DBUILD_LIBCINT=OFF"
   curl -L $1 | tar -C $src/pyscf/lib -xzf -
 else
-  export CMAKE_CONFIGURE_ARGS="-DWITH_F12=OFF"
+  MY_BUILD_ARGS="-DWITH_F12=OFF"
 fi
 
 # In certain versions of auditwheel, some .so files was excluded.
@@ -19,7 +19,7 @@ sed -i '/            if basename(fn) not in needed_libs:/s/basename.*libs/1/' /o
 # Compile wheels
 PYVERSION=cp39-cp39
 PYBIN=/opt/python/$PYVERSION/bin
-"${PYBIN}/pip" wheel -v --no-deps --no-clean -w /root/wheelhouse $src
+"${PYBIN}/pip" wheel -v --no-deps --no-clean --config-settings=cmake.args=${MY_BUILD_ARGS} -w /root/wheelhouse $src
 
 # Bundle external shared libraries into the wheels
 whl=`ls /root/wheelhouse/pyscf-*_x86_64.whl`
