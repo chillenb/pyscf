eb32e5b542e2c24e4fa1c8e7caf303269a353749
chillenb <chillenbrand15@gmail.com>
2026-02-28 22:07:57 -0500

    cmake overhaul

--- pyscf/lib/cmake/jobserver.cmake
+++
@@ -1,9 +0,0 @@
-# Add + to the Make recipe line if CMake is new enough.
-# This enables parallel compilation of the ExternalProjects that are
-# built using Make.
-# It does nothing if the generator isn't Make.
-# Tested with CMake<3.28 and >=3.28
-set(PYSCF_JOBSERVER_AWARE_ARG "")
-if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.28.0")
-  set(PYSCF_JOBSERVER_AWARE_ARG JOB_SERVER_AWARE TRUE)
-endif()
